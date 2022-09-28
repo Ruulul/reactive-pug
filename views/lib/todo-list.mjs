87a3510f1486ef75ca20a6dad3c94319983e02e5
Ruulul <vamjunior01@gmail.com>
2022-09-28 18:40:53 -0300

    better JSDocs

--- views/lib/todo-list.mjs
+++ views/lib/todo-list.mjs
@@ -1,24 +1,30 @@
 import Store from '/js/store.mjs';
 
+/**
+ * @typedef {{checked: Boolean, label: string, id: number}} Item
+ * @typedef {{items: Item[]}} State
+ * @returns {
+ *  store: import('./store.mjs').Store,
+ *  state: State,
+ * }
+ */
 function makeListStore() {
-    /**
-     * @typedef {{checked: Boolean, label: string, id: number}} Item
-     * @type {{items: Item[]}} list
-     */
-    const initial_state = {items: []};
-    /**@type {{store: import('./store.mjs').Store, state: {items: Item[]}}} */
-    const { store, state } = Store.makeStore(initial_state);
+    /**@type {{store: import('./store.mjs').Store, state: State}} */
+    const { store, state } = Store.makeStore({items: []});
     let id = 0;
     return {
         store,
         state,
+        /**@param {string} item item label*/
         addItem(item) {
             state.items.push({label: item, checked: false, id: id++});
             state.items = state.items;
         },
+        /**@param {number} id item id*/
         removeItem(id) {
             state.items = state.items.filter(item=>item.id!==id);
         },
+        /**@param {number} id item id*/
         markItem(id) {
             let item = store.state.items.find(item=>item.id===id)
             item.checked = !item.checked
